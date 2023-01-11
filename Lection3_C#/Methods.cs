c73352520c4e34ba19580fe6803442f6d520dfa7
Anton_Baranovsky <basmixer@gmail.com>
2023-01-11 22:05:00 +0300

    HW_Lection3_C# max algoritm done

---
+++ Lection3_C#/Methods.cs
@@ -0,0 +1,148 @@
+﻿/*
+// Вид 1 ничего не принимает и ничего не принимает
+void Method1()
+{
+    Console.WriteLine("Автор ...");
+}
+Method1();
+
+// Вид 2 ничего не возвращает но принимает
+
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2("Текст сообщения");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+{
+    Console.WriteLine(msg);
+    i++;
+}
+}
+//Method21("Текст", 4);
+Method21(count: 4, msg: "Текст"); // таким образом можно записывать именованые аргументы (не имеет значение последовательность)
+
+
+// Вид 3 что-то возвращает но не принимает
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+Console.WriteLine(year);
+
+// Вид 4 что-то принимает и что-то возвразщает
+
+string Method4 (int count, string text)
+{
+    string result = String.Empty;
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(10, "asd");
+Console.WriteLine(res);
+
+// Вывод таблицы умножения на экран
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <=10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+
+
+// ЗАДАЧА: Дан текст. В тексте нужно все пробелы заменить черточками, 
+// маленькие буквы "к" заменить большими буквами "К", а большие "С" заменить маленькими "с".
+
+string text = "– Я думаю, – сказал князь, улыбаясь, – что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+
+// string s = "qwerty"
+//             012
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
+
+*/
+
+// АЛГОРИТМ СОРТИРОВКИ min max
+// 1. НАйти позицию минимального элемента в неотсортированной части массива
+// 2. Произвести обмен этого значения со значением первой неотсортированной позиции
+// 3. Повторять пока есть не отсортированные элементы
+
+int[] arr ={1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+
+Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    //for (int i = 0; i < array.Length - 1; i++)
+    for (int i = 0; i < array.Length; i++)
+        {
+            //int minPosition = i;
+            int maxPosition = i;
+            
+            for (int j = i+1; j < array.Length; j++)
+            {
+                //if(array[j] < array[minPosition]) minPosition = j;
+                if(array[j] > array[maxPosition]) maxPosition = j;
+            }        
+            int temporary = array[i];
+            //array[i] = array[minPosition];
+            //array[minPosition] = temporary;
+            array[i] = array[maxPosition];
+            array[maxPosition] = temporary;
+        }
+}
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
